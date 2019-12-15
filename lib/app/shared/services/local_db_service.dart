ae9175c2301b176546be2d4815ba9ac64769df38
Matheus Torreão <mtorreao1@gmail.com>
2019-12-14 23:59:51 -0300

    LocalDBService

---
+++ lib/app/shared/services/local_db_service.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+class LocalDBService extends Disposable {
+  Completer<Box> completer = Completer<Box>();
+
+  LocalDBService(String collectionName) {
+    _initDB(collectionName);
+  }
+
+  Future<void> _initDB(String collectionName) async {
+    final directory = await path_provider.getApplicationDocumentsDirectory();
+    Hive.init(directory.path);
+    final box = await Hive.openBox(collectionName);
+    if (!completer.isCompleted) completer.complete(box);
+  }
+
+  Future<Box> getBox() async => await completer.future;
+
+  @override
+  void dispose() {
+    Hive.close();
+  }
+}
